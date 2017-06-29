f24a781790057367e4490030d79ec01223913fcb
sedo <iwish@hotmail.fr>
2017-06-29 21:42:16 +0200

    sf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
+    host: '217.70.180.130', 
     port:port
 });
 
